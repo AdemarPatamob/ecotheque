61527fd24917a6ef090479619ed50fad4196c844
Alexandre Tomaszewski <alextom27@gmail.com>
2018-12-20 18:05:39 +0100

    entity product ok

---
+++ src/Migrations/Version20181220164901.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181220164901 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, category VARCHAR(255) NOT NULL, brand VARCHAR(255) NOT NULL, need VARCHAR(255) NOT NULL, price INT NOT NULL, energy_class VARCHAR(255) NOT NULL, ges VARCHAR(255) NOT NULL, battery_movable TINYINT(1) DEFAULT NULL, battery_recyclability INT NOT NULL, screen_size INT NOT NULL, shut_down_auto TINYINT(1) DEFAULT NULL, autonomy INT DEFAULT NULL, descritption LONGTEXT DEFAULT NULL, image VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE product');
+    }
+}
