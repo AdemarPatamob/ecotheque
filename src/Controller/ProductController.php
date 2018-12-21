577e47563de6c3a70113bea7611b68caa41d06e3
Raphael Reboullet <raphaelreboullet@gmail.com>
2018-12-21 10:23:49 +0100

    modif recherche pour react

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -13,12 +13,6 @@ use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Serializer;
 use App\Form\ProductSearchType;
 use App\Form\ProductSearchByFiltersType;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Product;
-use Symfony\Component\HttpFoundation\Response;
-
 
 
 class ProductController extends AbstractController
@@ -27,90 +21,86 @@ class ProductController extends AbstractController
     //TODO merge barre de recherche , method index à inclure ici sans le form
     //TODO faire la requete en BDD via ProductRepository avec la valeur recupéré ici de model
 
+
     /**
      * @Route("/search", name = "search", methods = {"GET", "POST"})
      * @param Request $request
      * @return Response
      */
-    public function getSearch(Request $request) : Response
-    {
-        $data = $request->getContent();
-        $value = json_decode($data, 'true');
-        $response = new JsonResponse();
-        $response->setData($value['model']);
+        public function index(Request $request)
+        {
+            /*$products = $this->getDoctrine()
+                ->getRepository(Product::class)
+                ->findAll();
+            if (!$products) {
+                throw $this->createNotFoundException(
+                    'No article found in article\'s table.'
+                );
+            }*/
 
-     * @Route("/productsearch", name="product")
-     */
-    public function index(Request $request)
-    {
-        $products = $this->getDoctrine()
-            ->getRepository(Product::class)
-            ->findAll();
-        if (!$products) {
-            throw $this->createNotFoundException(
-                'No article found in article\'s table.'
-            );
-        }
-
-        $form = $this->createForm(
-            ProductSearchType::class,
-            null,
-            ['method' => Request::METHOD_GET]
-        );
-        $form->handleRequest($request);
-        if ($form->isSubmitted()) {
-            $data = $form->getData();
-            // $data contient les données du $_POST
-            // Faire une recherche dans la BDD avec les infos de $data...
+            $data = $request->getContent();
+            $value = json_decode($data, 'true');
+            $response = new JsonResponse();
+            $response->setData($value['model']);
             $products = $this->getDoctrine()->getRepository(Product::class)
-                ->findLike($data['searchField']);
-        }
+                ->findLike($value['model']);
 
 
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController', 'products' => $products,
-            'form' => $form->createView()
-        ]);
-    }
-
-    /**
-     * @Route("/productsearchbyfilter", name="productbyfilter")
-     */
-    public function index2(Request $request)
-    {
-        $products = $this->getDoctrine()
-            ->getRepository(Product::class)
-            ->findAll();
-        if (!$products) {
-            throw $this->createNotFoundException(
-                'No article found in article\'s table.'
+            /*$form = $this->createForm(
+                ProductSearchType::class,
+                null,
+                ['method' => Request::METHOD_GET]
             );
+            $form->handleRequest($request);
+            if ($form->isSubmitted()) {
+                $data = $form->getData();
+                // $data contient les données du $_POST
+                // Faire une recherche dans la BDD avec les infos de $data...*/
+
+
+            return $this->render('product/index.html.twig', [
+                'controller_name' => 'ProductController', 'products' => $products,
+            ]);
         }
 
-
-        $formfilter = $this->createForm(
-            ProductSearchByFiltersType::class,
-            null,
-            ['method' => Request::METHOD_GET]
-        );
-        $formfilter->handleRequest($request);
-        if ($formfilter->isSubmitted()) {
-            $data = $formfilter->getData();
-            // $data contient les données du $_POST
-            // Faire une recherche dans la BDD avec les infos de $data...
-            $products = $this->getDoctrine()->getRepository(Product::class)
-                ->findLikeFilter($data['category'], $data['brand'], $data['energyClass'], $data['ges'], $data['need']);
-
-
-        }
+       /* /**
+         * @Route("/productsearchbyfilter", name="productbyfilter")
+         */
+       /* public function index2(Request $request)
+        {
+            $products = $this->getDoctrine()
+                ->getRepository(Product::class)
+                ->findAll();
+            if (!$products) {
+                throw $this->createNotFoundException(
+                    'No article found in article\'s table.'
+                );
+            }
 
 
+            $formfilter = $this->createForm(
+                ProductSearchByFiltersType::class,
+                null,
+                ['method' => Request::METHOD_GET]
+            );
+            $formfilter->handleRequest($request);
+            if ($formfilter->isSubmitted()) {
+                $data = $formfilter->getData();
+                // $data contient les données du $_POST
+                // Faire une recherche dans la BDD avec les infos de $data...
+                $products = $this->getDoctrine()->getRepository(Product::class)
+                    ->findLikeFilter($data['category'], $data['brand'], $data['energyClass'], $data['ges'], $data['need']);
 
-        return $this->render('product/index2.html.twig', [
-            'controller_name' => 'ProductController', 'products' => $products,
-            'formfilter' => $formfilter->createView()
+
+            }
 
 
-        return $response;
+            return $this->render('product/index2.html.twig', [
+                'controller_name' => 'ProductController', 'products' => $products,
+                'formfilter' => $formfilter->createView()];
+
+
+
     }
+    }*/
 }
